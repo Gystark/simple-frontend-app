677ae96093715137cb061799d4db442cd6e1da82
Gabor Borics-Kurti <gystark@gmail.com>
2020-02-08 16:00:02 +0000

    Created a mock backend

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+// Based on https://github.com/typicode/json-server
+
+const jsonServer = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+const VALID_AUTH_TOKEN = "valid-auth-token";
+const UNAUTHORISED_ROUTES = ["/login"];
+
+function allowUnauthorised(req) {
+    return UNAUTHORISED_ROUTES.includes(req.path)
+}
+
+function isAuthorized(req) {
+    const authHeader = req.header("Authorisation");
+    if (authHeader !== undefined) {
+        if (authHeader === VALID_AUTH_TOKEN) {
+            return true;
+        }
+    }
+    return false;
+}
+
+server.use(middlewares);
+server.use((req, res, next) => {
+    if (allowUnauthorised(req) || isAuthorized(req)) {
+        next();
+    } else {
+        res.status(401).json({"message": "Unauthorised"});
+    }
+});
+
+server.use(jsonServer.bodyParser);
+server.post("/login", (req, res) => {
+    if (req.body.username === "test_user" && req.body.password === "test_password") {
+        res.json({"token": VALID_AUTH_TOKEN});
+    } else {
+        res.status(401).json({"message": "Incorrect username or password"})
+    }
+});
+
+server.use(router);
+
+server.listen(3001, () => {
+    console.log('Listening...')
+});
